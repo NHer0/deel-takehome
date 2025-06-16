9d172cd3680788f0e23fc519d65cd191ed95d768
NHer0 <91466737+NHer0@users.noreply.github.com>
2025-06-16 09:25:02 +0200

    Small fix in dim

--- dbt/deel_takehome/models/dimensions/dim__organizations.sql
+++ dbt/deel_takehome/models/dimensions/dim__organizations.sql
@@ -15,20 +15,13 @@ with organizations_context as (
         first_payment_date,
         last_payment_date,
         created_at_utc,
-        datediff('day', created_at_utc, current_date) as organization_age_days,
         datediff('day', last_payment_date, current_date) as days_since_last_payment,
         case 
             when count_total_contracts_active = 1 then 'single_contract'
             when count_total_contracts_active <= 5 then 'small_portfolio'
             when count_total_contracts_active <= 20 then 'medium_portfolio'
             else 'large_portfolio'
-        end as contract_portfolio_size,
-        case 
-            when datediff('day', created_at_utc, current_date) <= 30 then 'new'
-            when datediff('day', created_at_utc, current_date) <= 180 then 'growing'
-            when datediff('day', created_at_utc, current_date) <= 365 then 'established'
-            else 'mature'
-        end as organization_maturity
+        end as contract_portfolio_size
     from {{ stg_organizations }}
 )
 
